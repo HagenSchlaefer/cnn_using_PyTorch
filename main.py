7c9bc266c8693641ff6caa3d2837cede5550baa3
HagenSchlaefer <hagen@sleeper.de>
2026-02-01 20:25:03 +0100

    change 010220262024

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from MyCnn import ConvNet
 from myCnn2 import ConvNet as ConvNet2
 from cnn import train, plot_metrics, run
-from data import visualize_activations
+from data import save_activations, visualize_activations
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -29,19 +29,29 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 model.load_state_dict(torch.load(f'cnn_epoch{num_epochs}.pth'))
 model.eval()
 
-img_path = f"../TestData/mama7.png"
+img_path = f"../TestData/hina9.png"
 pred, model = run(model, device, img_path)
 print(f"Predicted label: {pred}")
 
-visualize_activations(model.conv1_x, "conv1")
-visualize_activations(model.convStride1_x, "convStride1")
-visualize_activations(model.conv2_x, "conv2")
-visualize_activations(model.convStride2_x, "convStride2")
+# visualize_activations(model.conv1_x, "conv1")
+# visualize_activations(model.convStride1_x, "convStride1")
+# visualize_activations(model.conv2_x, "conv2")
+# visualize_activations(model.convStride2_x, "convStride2")
 
-visualize_activations(model.view_x, "flatten")
-visualize_activations(model.fc1_x, "fc1")
-visualize_activations(model.fc2_x, "fc2")
-visualize_activations(model.fc3_x, "fc3")
+# visualize_activations(model.view_x, "flatten")
+# visualize_activations(model.fc1_x, "fc1")
+# visualize_activations(model.fc2_x, "fc2")
+# visualize_activations(model.fc3_x, "fc3")
+
+save_activations(model.conv1_x, "1_conv1")
+save_activations(model.convStride1_x, "2_convStride1")
+save_activations(model.conv2_x, "3_conv2")
+save_activations(model.convStride2_x, "4_convStride2")
+
+save_activations(model.view_x, "5_flatten")
+save_activations(model.fc1_x, "6_fc1")
+save_activations(model.fc2_x, "7_fc2")
+save_activations(model.fc3_x, "8_fc3")
 
 # for i in range(10):
 #     if i == 4:
