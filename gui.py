0f2ed339c5b0d709d0de1e4142de96264e6928de
HagenSchlaefer <hagen@sleeper.de>
2026-02-23 17:26:29 +0100

    ui Cours

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QTextEdit, QSlider, QProgressBar, QComboBox, QListWidget, QRadioButton, QCheckBox
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QTextEdit, QSlider, QProgressBar, QComboBox, QListWidget, QRadioButton, QCheckBox, QMessageBox
 from PySide6.QtCore import Qt
 from torch import layout
 
@@ -8,13 +8,39 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Hello World Application")
 
+        menubar = self.menuBar()
+
+        fileMenu = menubar.addMenu("File")
+        editMenu = menubar.addMenu("Edit")  
+        helpMenu = menubar.addMenu("Help")
+
+        supMenu = fileMenu.addMenu("Submenu")
+        exitAction = supMenu.addAction("Exit")
+        exitAction.triggered.connect(self.close)
+
+        aboutAction = helpMenu.addAction("About")
+        aboutAction.triggered.connect(lambda: print("This is a simple PySide6 application."))
+
         container = QWidget()
         inner_container = QWidget()
 
         self.setCentralWidget(container)
 
+        self.count = 1
+        self.windows = []
+
         layout = QVBoxLayout(container)
         inner_layout = QHBoxLayout(inner_container)
+
+        buttonSW = QPushButton("Open Secondary Window")
+        buttonSW.clicked.connect(self.open_secondary_window)
+       
+
+        button = QPushButton("Show Message")
+        button.clicked.connect(lambda: QMessageBox.information(self, "Info", "Hello World"))
+
+        buttonY = QPushButton("Show Choices")
+        buttonY.clicked.connect(self.ask_choices)
 
         # add a label to the main window
         label = QLabel("One")
@@ -43,6 +69,10 @@ class MainWindow(QMainWindow):
         inner_layout.addWidget(radio2)
         inner_layout.addWidget(radio3)
 
+        layout.addWidget(button)
+        layout.addWidget(buttonY)
+        layout.addWidget(buttonSW)
+
         layout.addWidget(label)
         layout.addWidget(button1)
         layout.addWidget(button2)   
@@ -57,9 +87,48 @@ class MainWindow(QMainWindow):
         if r.isChecked():
             print("Radio button was checked:", r.text())
      
+    def ask_choices(self):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("Choice")
+        msg_box.setText("Choose an option:")
+
+        msg_box.addButton("Python", QMessageBox.AcceptRole)
+        msg_box.addButton("C++", QMessageBox.AcceptRole)
+        msg_box.addButton("JavaScript", QMessageBox.AcceptRole)
+
+        msg_box.exec()
+
+        if msg_box.clickedButton().text() == "Python":
+            print("User chose Python!")
+        elif msg_box.clickedButton().text() == "C++":
+            print("User chose C++!")
+        elif msg_box.clickedButton().text() == "JavaScript":
+            print("User chose JavaScript!")
         
-  
+    def ask_yes_no(self):
+        reply = QMessageBox.question(self, "Question", "Do you like PySide6?", QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            print("User likes PySide6!")
+        else:
+            print("User does not like PySide6.")
         
+    def open_secondary_window(self):
+        w = SecondaryWindow(self.count)
+        self.count += 1
+        self.windows.append(w)  # keep a reference 
+        w.show()
+
+class SecondaryWindow(QMainWindow):
+    def __init__(self, n):
+        super().__init__()
+
+        self.setWindowTitle(f"Window Number {n}")
+
+        label = QLabel(f"Number {n}")
+        label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(label)
+
 app = QApplication()
 
 window = MainWindow()
